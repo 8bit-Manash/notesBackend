5a2f9068192f8edbaf1acabd571a94a7edab42f7
8bit-Manash <manastiwary2067@gmail.com>
2025-12-26 16:05:27 +0530

    Refactor note retrieval and enhance update/delete functionality in routes

--- routes.py
+++ routes.py
@@ -4,6 +4,9 @@ from fastapi import APIRouter
 # Import Note model for data validation
 from models import Note
 
+# bson -> binary JSON
+from bson import ObjectId
+
 # Import MongoDB collection to store and fetch notes
 from database import notes_collection
 
@@ -27,7 +30,39 @@ def get_notes():
         # Fetch all documents from MongoDB
     # {} means fetch all records
     # {"_id": 0} removes MongoDB auto-generated _id field
-    for note in notes_collection.find({}, {"_id": 0}):
+    for note in notes_collection.find():
         # Add each note into list
-        notes.append(note)
+        notes.append(
+            {
+                "title": note["title"],
+                "content": note["content"],
+                "id": str(note["_id"])
+            }
+        )
     return notes
+
+
+@router.put("/notes/{note_id}")
+def update_note(note_id: str, note: Note):
+    # Convert note object into dictionary and insert into MongoDB JSON → Note object → dict → MongoDB
+    print("ID coming from frontend:", note_id)
+    print("Type of ID:", type(note_id))
+
+    result = notes_collection.update_one(
+        {"_id": ObjectId(note_id)},
+        {
+            "$set": {
+                "title": note.title,
+                "content": note.content
+            }
+        }
+    )
+    return {"message": "Note updated"}
+
+@router.delete("/notes/{note_id}")
+def delete_note(note_id: str):
+    result = notes_collection.delete_one({"_id": ObjectId(note_id)})
+    if result.deleted_count == 1:
+        return {"message": "Note deleted"}
+    else:
+        return {"message": "Note not found"}
